6631b622745a420774cf9ba0cac2d366629876f2
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-06-15 16:52:00 +0700

    feat: HeaderSettings $$ankiConnect unresolved

--- packages/note/src/lazy/components/HeaderSettings.tsx
+++ packages/note/src/lazy/components/HeaderSettings.tsx
@@ -2,7 +2,7 @@ import { useNavigationTransition } from "#/src/hooks/transition";
 import { useGeneralContext } from "#/src/contexts/GeneralContext";
 import { HeaderLayout } from "./HeaderLayout";
 import { ArrowLeftIcon, RefreshCwIcon } from "./Icons";
-import { Match, Switch } from "solid-js";
+import { Match, Show, Switch } from "solid-js";
 
 export function HeaderSettings() {
   const { $general, $checkAnkiConnect, $$ankiConnect } = useGeneralContext();
@@ -23,21 +23,28 @@ export function HeaderSettings() {
           <Match when={$$ankiConnect.loading}>
             <div class="text-sm text-base-content-calm">Checking AnkiConnect...</div>
           </Match>
-          <Match when={$$ankiConnect.error}>
-            <button
-              on:click={async () => {
-                await $checkAnkiConnect({
-                  onFail: () => {
-                    $general.toast.error("AnkiConnect is not available");
-                  },
-                });
-              }}
-              on:touchend={(e) => e.stopPropagation()}
-            >
-              <RefreshCwIcon class="size-4 cursor-pointer text-base-content-soft" />
-            </button>
-            <div class="text-sm text-base-content-calm">AnkiConnect is not available</div>
-            <div class="status status-error animate-ping"></div>
+          <Match when={$$ankiConnect.error || $$ankiConnect.state === "unresolved"}>
+            <div class="flex items-center gap-0.5">
+              <div class="indicator">
+                <button
+                  on:click={async () => {
+                    await $checkAnkiConnect({
+                      onFail: () => {
+                        $general.toast.error("AnkiConnect is not available");
+                      },
+                    });
+                  }}
+                  on:touchend={(e) => e.stopPropagation()}
+                >
+                  <RefreshCwIcon class="size-4 cursor-pointer text-base-content-soft" />
+                </button>
+                <span class="status status-error animate-ping"></span>
+              </div>
+
+              <Show when={$$ankiConnect.error}>
+                <div class="text-sm text-base-content-calm">AnkiConnect is not available</div>
+              </Show>
+            </div>
           </Match>
           <Match when={$$ankiConnect.state === "ready"}>
             <div class="text-sm text-base-content-calm">AnkiConnect is available</div>
